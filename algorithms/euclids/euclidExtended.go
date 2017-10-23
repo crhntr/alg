df7a87b60a5e261be9f3c5ed94b5adf763a2bef4
crhntr <hunter.christopher@icloud.com>
2017-10-22 21:33:10 -0700

    An attempt at the extended algorithm

---
+++ algorithms/euclids/euclidExtended.go
@@ -0,0 +1,25 @@
+package euclid
+
+// ExtendedEuclid based on Pseudocode from
+// wikipedia:
+// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+func ExtendedEuclid(a, b int) (
+	BézoutCoefficientsX, BézoutCoefficientsY int,
+	gcd int,
+	quotientsByGCDa, quotientsByGCDb int) {
+	// m, n, r := a, b, a%b
+	s, sPrevious := 0, 1
+	t, tPrevious := 1, 0
+	r, rPrevious := b, a
+
+	for r > 0 {
+		quotient := rPrevious / r
+		rPrevious, r = r, rPrevious-quotient*r
+		sPrevious, s = s, sPrevious-quotient*s
+		tPrevious, t = t, tPrevious-quotient*t
+	}
+
+	return sPrevious, tPrevious,
+		rPrevious,
+		t, s
+}
