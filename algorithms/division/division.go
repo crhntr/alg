6a740432d4a763cec236e1bd625217daaacf6a7b
crhntr <hunter.christopher@icloud.com>
2017-10-23 13:34:20 -0700

    Removed A few new lines

--- algorithms/division/division.go
+++ algorithms/division/division.go
@@ -18,8 +18,6 @@ func DivisionVerbose(x, y int) (q, r int) {
 		fmt.Printf("Loop Invariant: x = (q*y) + r ⋏ r ≥ 0: %t\n", x == (q*y)+r && r >= 0)
 		r, q = r-y, q+1
 	}
-
 	fmt.Printf("Postcondition: x=(q*y)+r ⋏ 0 ≤ r < y: %t\n", x == (q*y)+r && 0 <= r && r < y)
-
 	return
 }
