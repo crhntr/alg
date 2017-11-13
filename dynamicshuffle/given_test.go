99540d5f493c9f6e46098370ede234809dc0d300
crhntr <hunter.christopher@icloud.com>
2017-11-12 19:34:07 -0800

    More Benchmarks :)

--- dynamicshuffle/given_test.go
+++ dynamicshuffle/given_test.go
@@ -101,11 +101,20 @@ func BenchmarkIsShuffleRecursive12(b *testing.B) {
 func BenchmarkIsShuffleRecursive120(b *testing.B) {
 	benchmarkShuffle(generateShuffle(60, 60, alphabet), IsShuffleRecursive, b)
 }
+func BenchmarkIsShuffleRecursive240(b *testing.B) {
+	benchmarkShuffle(generateShuffle(120, 120, alphabet), IsShuffleRecursive, b)
+}
 func BenchmarkIsShuffleRecursive1200(b *testing.B) {
 	benchmarkShuffle(generateShuffle(600, 600, alphabet), IsShuffleRecursive, b)
 }
+func BenchmarkIsShuffleRecursive2400(b *testing.B) {
+	benchmarkShuffle(generateShuffle(1200, 1200, alphabet), IsShuffleRecursive, b)
+}
 func BenchmarkIsShuffleRecursive12000(b *testing.B) {
 	benchmarkShuffle(generateShuffle(6000, 6000, alphabet), IsShuffleRecursive, b)
+}
+func BenchmarkIsShuffleRecursive24000(b *testing.B) {
+	benchmarkShuffle(generateShuffle(12000, 12000, alphabet), IsShuffleRecursive, b)
 }
 func BenchmarkIsShuffleRecursive120000(b *testing.B) {
 	benchmarkShuffle(generateShuffle(60000, 60000, alphabet), IsShuffleRecursive, b)
@@ -125,10 +134,10 @@ func generateShuffle(uLen, vLen int, Σ string) []string {
 
 	for ui, vi := 0, 0; ui+vi < (uLen + vLen); {
 		if rand.Intn(2) == 1 {
-			u += u[ui : ui+1]
+			w += u[ui : ui+1]
 			ui++
 		} else {
-			v += v[vi : vi+1]
+			w += v[vi : vi+1]
 			vi++
 		}
 
