8fff812ec81dfc4ec851e8749a255fb320e2c694
crhntr <hunter.christopher@icloud.com>
2017-11-26 23:04:36 -0800

    Fixed Indexing to Remove +1 Hack

--- dynamicshuffle/iterative.py
+++ dynamicshuffle/iterative.py
@@ -7,21 +7,14 @@ def isShuffleIterative(w, u, v): # returns a boolean
     if len(v) == 0:
         return u == w
     table = [[False for j in range(len(v)+1)] for i in range((len(u)+1))]
-
     table[0][0] = True
     for i in range(len(u)):
-        i+=1
-        table[i][0] = table[i-1][0] and (w[i-1] == u[i-1])
-
+        table[i+1][0] = table[i][0] and (w[i] == u[i])
     for j in range(len(v)):
-        j+=1
-        table[0][j] = table[0][j-1] and (w[j-1] == v[j-1])
-
+        table[0][j+1] = table[0][j] and (w[j] == v[j])
     for i in range(len(u)):
-        i+=1
         for j in range(len(v)):
-            j+=1
-            table[i][j] = (w[i+j-1] == u[i-1] and table[i-1][j]) or (w[i+j-1] == v[j-1] and table[i][j-1])
+            table[i+1][j+1] = (w[i+j+1] == u[i] and table[i][j+1]) or (w[i+j+1] == v[j] and table[i+1][j])
     return table[len(u)][len(v)]
 
 if not isShuffleIterative("010101", "000", "111"):
