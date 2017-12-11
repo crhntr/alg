41e61a876c2a3299ae23b957109da04f66f6b47a
crhntr <hunter.christopher@icloud.com>
2017-12-10 17:36:38 -0800

    Added Problem 4.4

--- longestmonotonesequence/alg.go
+++ longestmonotonesequence/alg.go
@@ -26,3 +26,38 @@ func Len(seq []int) int {
 	}
 	return max
 }
+
+func Dif(seq []int, difference int) int {
+	lmsAtJ := make([]int, len(seq))
+	for i := range lmsAtJ {
+		lmsAtJ[i] = 1
+	}
+
+	for j := 1; j < len(seq); j++ {
+		max := 0
+
+		for i := 0; i < j; i++ {
+
+			if lmsAtJ[i] > max {
+				dif := seq[i] - seq[j]
+				if dif < 0 {
+					dif *= -1
+				}
+
+				if dif <= difference {
+					max = lmsAtJ[i]
+				}
+			}
+		}
+
+		lmsAtJ[j] = max + 1
+	}
+
+	max := 1
+	for _, ln := range lmsAtJ {
+		if ln > max {
+			max = ln
+		}
+	}
+	return max
+}
