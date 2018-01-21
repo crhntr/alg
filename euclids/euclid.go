39394bee2f779c6f61a0b72fc9085541862c6613
crhntr <hunter.christopher@icloud.com>
2018-01-20 19:22:05 -0800

    Improved Package Organization and Documentation

--- euclids/euclid.go
+++
@@ -1,9 +0,0 @@
-package euclid
-
-func Euclid(a, b int) (n int) {
-	m, n, r := a, b, a%b
-	for r > 0 {
-		m, n, r = n, r, m%n
-	}
-	return // n
-}
--- euclids/euclidExtended.go
+++ euclids/euclidExtended.go
@@ -1,10 +1,11 @@
-package euclid
+package euclids
 
-// ExtendedEuclid based on Pseudocode from
+// Extended based on Pseudocode from
 // wikipedia:
 // https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-func ExtendedEuclid(a, b int) (BézoutCoefficientsX, BézoutCoefficientsY, gcd, quotientsByGCDa, quotientsByGCDb int) {
-	// m, n, r := a, b, a%b
+// Returned ingeter names used to express the semantics each of the
+// returned integers.
+func Extended(a, b int) (BézoutCoefficientsX, BézoutCoefficientsY, gcd, quotientsByGCDa, quotientsByGCDb int) {
 	s, sPrevious := 0, 1
 	t, tPrevious := 1, 0
 	r, rPrevious := b, a
