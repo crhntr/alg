6d622a57b56642c2d63799cdd2068b5bed37fb82
crhntr <hunter.christopher@icloud.com>
2018-01-20 19:15:11 -0800

    Reformated args and return onto single line

--- euclids/euclidExtended.go
+++ euclids/euclidExtended.go
@@ -3,10 +3,7 @@ package euclid
 // ExtendedEuclid based on Pseudocode from
 // wikipedia:
 // https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-func ExtendedEuclid(a, b int) (
-	BézoutCoefficientsX, BézoutCoefficientsY int,
-	gcd int,
-	quotientsByGCDa, quotientsByGCDb int) {
+func ExtendedEuclid(a, b int) (BézoutCoefficientsX, BézoutCoefficientsY, gcd, quotientsByGCDa, quotientsByGCDb int) {
 	// m, n, r := a, b, a%b
 	s, sPrevious := 0, 1
 	t, tPrevious := 1, 0
@@ -19,7 +16,5 @@ func ExtendedEuclid(a, b int) (
 		tPrevious, t = t, tPrevious-quotient*t
 	}
 
-	return sPrevious, tPrevious,
-		rPrevious,
-		t, s
+	return sPrevious, tPrevious, rPrevious, t, s
 }
