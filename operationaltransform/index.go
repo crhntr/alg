6dbd35ee2d0423d7b983c235ddbc9b03cadf35f6
crhntr <hunter.christopher@icloud.com>
2018-04-26 23:57:55 -0700

    Added Cookie with sid

--- operationaltransform/index.go
+++ operationaltransform/index.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/http"
+	"time"
+
+	"github.com/globalsign/mgo/bson"
 )
 
 func serveIndex(w http.ResponseWriter, r *http.Request) {
@@ -13,5 +16,8 @@ func serveIndex(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	cookie := http.Cookie{Name: "sid", Value: bson.NewObjectId().Hex(), Expires: time.Now().Add(365 * 24 * time.Hour)}
+
+	http.SetCookie(w, &cookie)
 	http.ServeFile(w, r, "index.html")
 }
